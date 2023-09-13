03f235464eeb4e629168e368aeecaed12d79c330
mlwelker <nutria.watts.0g@icloud.com>
2023-09-13 16:44:35 -0500

    accessing the documents directory

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -1,29 +1,27 @@
 
 import SwiftUI
 
-struct User: Identifiable, Comparable {
-    let id = UUID()
-    let firstName: String
-    let lastName: String
-    
-    static func <(lhs: User, rhs: User) -> Bool {
-        lhs.lastName < rhs.lastName
-    }
-}
-
 struct ContentView: View {
-    let users = [
-        User(firstName: "Arnold", lastName: "Rimmer"),
-        User(firstName: "Kristine", lastName: "Kochanski"),
-        User(firstName: "David", lastName: "Lister"),
-    ].sorted {
-        $0.lastName < $1.lastName
+    var body: some View {
+        Text("hello world")
+            .onTapGesture {
+                let str = "Test message"
+                let url = getDocumentsDirectory().appendingPathComponent("message.txt")
+                
+                do {
+                    try str.write(to: url, atomically: true, encoding: .utf8)
+                    
+                    let input = try String(contentsOf: url)
+                    print(input)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
     }
     
-    var body: some View {
-        List(users) { user in
-            Text("\(user.firstName) \(user.lastName)")
-        }
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
 }
 

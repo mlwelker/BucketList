288fb0ff225916c53410bb51a7a171f053543ee1
mlwelker <nutria.watts.0g@icloud.com>
2023-09-13 16:49:51 -0500

    rect or circle view + loading state enum & views

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -1,27 +1,37 @@
 
 import SwiftUI
 
-struct ContentView: View {
+enum LoadingState {
+    case loading, success, failed
+}
+
+struct LoadingView: View {
     var body: some View {
-        Text("hello world")
-            .onTapGesture {
-                let str = "Test message"
-                let url = getDocumentsDirectory().appendingPathComponent("message.txt")
-                
-                do {
-                    try str.write(to: url, atomically: true, encoding: .utf8)
-                    
-                    let input = try String(contentsOf: url)
-                    print(input)
-                } catch {
-                    print(error.localizedDescription)
-                }
-            }
+        Text("loading...")
     }
+}
+
+struct SuccessView: View {
+    var body: some View {
+        Text("success")
+    }
+}
+
+struct FailedView: View {
+    var body: some View {
+        Text("failed")
+    }
+}
+
+struct ContentView: View {
+    var loadingState = LoadingState.loading
     
-    func getDocumentsDirectory() -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        return paths[0]
+    var body: some View {
+        if Bool.random() {
+            Rectangle()
+        } else {
+            Circle()
+        }
     }
 }
 

3e1d62f950846741ad32cef26d369a91a5ff14c1
mlwelker <nutria.watts.0g@icloud.com>
2023-09-13 08:30:29 -0500

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Michael Welker on 2023-09-13.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

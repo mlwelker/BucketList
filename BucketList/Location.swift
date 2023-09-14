2d2f403b78b240f3101b9999c9d21cc7a522508e
mlwelker <nutria.watts.0g@icloud.com>
2023-09-13 23:42:04 -0500

    adding user locations to map

---
+++ BucketList/Location.swift
@@ -0,0 +1,10 @@
+
+import Foundation
+
+struct Location: Identifiable, Codable, Equatable {
+    let id: UUID
+    var name: String
+    var description: String
+    let latitude: Double
+    let longitude: Double
+}
